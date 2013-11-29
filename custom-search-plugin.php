4fa9021fe98ae62bad67e89077aa385c7512810f
bestwebsoft <bestwebsoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-29 08:07:02 +0000

    custom-search-plugin.php file is updated.
    git-svn-id: https://plugins.svn.wordpress.org/custom-search-plugin/tags/1.17@812283 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-search-plugin.php
+++ custom-search-plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Custom Search
 Plugin URI: http://bestwebsoft.com/plugin/
 Description: Custom Search Plugin designed to search for site custom types.
 Author: BestWebSoft
-Version: 1.16
+Version: 1.17
 Author URI: http://bestwebsoft.com/
 License: GPLv2 or later
 */
